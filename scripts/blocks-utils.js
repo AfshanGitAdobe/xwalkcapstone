725c2b4ea05242db67690fd03aadd471213f9cf4
AfshanGitAdobe <57437737+AfshanGitAdobe@users.noreply.github.com>
2025-03-03 12:54:21 +0530

    Create blocks-utils.js

---
+++ scripts/blocks-utils.js
@@ -0,0 +1,29 @@
+/**
+ * Create new DOM element with tag name and class name.
+ * @param tagName tag name
+ * @param className class name
+ * @returns created element
+ */
+function createElement(tagName, className) {
+  const element = document.createElement(tagName);
+  if (className) {
+    element.classList.add(className);
+  }
+  return element;
+}
+
+/**
+ * Open Search Bar
+ * @param cb callback function to be called when search bar is closed
+ * @returns {Promise<void>}
+ */
+function openSearchBar(cb, arg) {
+  const searchBlock = document.querySelector('.search.block');
+  if (searchBlock) {
+    const searchSection = searchBlock.closest('.section');
+    document.querySelector('main').prepend(searchSection);
+    searchBlock.style.display = 'block';
+    searchBlock.querySelector('input')?.focus();
+    searchCb = { cb, arg };
+  }
+}
